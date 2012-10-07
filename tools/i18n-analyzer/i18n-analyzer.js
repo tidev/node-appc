5a849dfb1e3a96b521fcc241747fc1212e5efc9d
Bryan Hughes <bhughes@appcelerator.com>
2012-10-07 14:19:31 -0700

    [TIMOB-11229] Finished i18n analyzer

--- tools/i18n-analyzer/i18n-analyzer.js
+++ tools/i18n-analyzer/i18n-analyzer.js
@@ -1,19 +1,33 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
 
 var path = require('path'),
 	fs = require('fs'),
 	util = require('util'),
 	uglify = require('uglify-js'),
 	wrench = require('wrench'),
+	
 	sourceDir = process.argv[2],
-	destinationFile = process.argv[3] || 'i18nStrings.json',
+	localesDir = process.argv[3],
+	
 	jsExtensionRegex = /\.js$/,
+	localeDirRegex = /^locales$/,
+	nodeModulesRegex = /node_modules/,
+	
 	startTime = Date.now(),
-	i18nStrings = [],
-	i18nPluralStrings = [];
+	
+	en = {},
+	files,
+	file,
+	
+	i = 0, len,
+	
+	locales,
+	locale,
+	localePath,
+	localeName;
 
 if (!sourceDir) {
-	console.log('Usage: i18n-analyzer.js <source-dir> [<destination-file>]\n');
+	console.log('Usage: i18n-analyzer.js <source-dir> <locales-dir>]\n');
 	process.exit(1);
 }
 
@@ -21,20 +35,23 @@ if (!fs.existsSync(sourceDir) || !fs.lstatSync(sourceDir).isDirectory()) {
 	console.log('"' + sourceDir + '" does not exist or is not a directory\n');
 	process.exit(1);
 }
+sourceDir = path.resolve(sourceDir);
 
-var files = wrench.readdirSyncRecursive(sourceDir),
-	file,
-	i = 0,
-	len = files.length;
+// Process the source files
+files = wrench.readdirSyncRecursive(sourceDir);
+len = files.length;
 for(; i < len; i++) {
-	file = path.resolve(path.join(sourceDir, files[i]));
-	if (jsExtensionRegex.test(file)) {
+	file = path.join(sourceDir, files[i]);
+	if (jsExtensionRegex.test(file) && !localeDirRegex.test(path.dirname(files[i])) && !nodeModulesRegex.test(files[i])) {
 		processFile(file);
 	}
 }
 
 function processFile(file) {
-	var ast = uglify.parser.parse(fs.readFileSync(file).toString(), false, true),
+
+	console.log('Processing ' + file);
+	
+	var ast,
 		i18n = [],
 		nodeStack = [],
 		node,
@@ -42,8 +59,13 @@ function processFile(file) {
 		i, len,
 		nodesProcessed = 0,
 		stringsFound = 0;
-
-	console.log('Processing ' + file);
+	
+	try {
+		ast = uglify.parser.parse(fs.readFileSync(file).toString(), false, true);
+	} catch (e) {
+		console.error('Source file ' + file + ' could not be parsed: ' + e.message);
+		return;
+	}
 	
 	// Append one or more AST nodes
 	function appendNode() {
@@ -83,9 +105,15 @@ function processFile(file) {
 				}
 				stringsFound++;
 				if (node[1][1] === '__') {
-					i18nStrings.push(node[2][0][1]);
+					en[node[2][0][1]] = node[2][0][1];
 				} else {
-					i18nPluralStrings.push(node[2][0][1]);
+					if (node[2][1][0].name !== 'string') {
+						console.warn('Non-string found in i18n call');
+					}
+					en[node[2][0][1]] = {
+						one: node[2][0][1],
+						other: node[2][1][1]
+					}
 				}
 			}
 		}
@@ -220,10 +248,48 @@ function processFile(file) {
 	console.log(nodesProcessed + ' nodes processed\n');
 }
 
-fs.writeFileSync(destinationFile, JSON.stringify({
-	__: i18nStrings,
-	__n: i18nPluralStrings
-}, false, '\t'));
+// Write the en locale file
+console.log('Creating en locale file\n');
+localesDir = localesDir ? path.resolve(localesDir) : path.join(sourceDir, 'locales');
+if (!fs.existsSync(localesDir)) {
+	wrench.mkdirSyncRecursive(localesDir);
+}
+fs.writeFileSync(path.join(localesDir, 'en.js'), JSON.stringify(en, false, '\t'));
 
+// Validate the other locales, if any exist
+locales = wrench.readdirSyncRecursive(localesDir);
+for(i = 0, len = locales.length; i < len; i++) {
+	localeName = locales[i].replace('.js', '');
+	if (localeName !== 'en') {
+		
+		console.log('Validating local ' + localeName);
+		
+		localePath = path.join(localesDir, localeName + '.js');
+		try {
+			locale = JSON.parse(fs.readFileSync(localePath));
+		} catch (e) {
+			console.error('Locale file ' + localePath + ' is not valid');
+			continue;
+		}
+		
+		// Remove unused entries
+		Object.keys(locale).forEach(function(entry) {
+			if (!en[entry]) {
+				delete locale[entry];
+				console.log('Removing unused entry "' + entry + '"');
+			}
+		});
+		
+		// Make sure all used entries are present
+		Object.keys(en).forEach(function(entry) {
+			if (!locale[entry]) {
+				console.error('**** Locale ' + localeName + ' is missing entry "' + entry + '" ****');
+			}
+		});
+		
+		fs.writeFileSync(path.join(localesDir, localeName + '.js'), JSON.stringify(locale, true, '\t'));
+		console.log();
+	}
+}
 
 console.log('Analysis completed in ' + ((Date.now() - startTime) / 1000) + 's\n');
