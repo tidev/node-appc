d296c790573f4d2426b343b8a6cd9152fff8c7ed
Bryan Hughes <bhughes@appcelerator.com>
2012-11-19 14:53:33 -0800

    [TIMOB-11825] Updated the locale files from Web Translate It.

--- locales/en.js
+++ locales/en.js
@@ -3,14 +3,14 @@
 	"Copying %s => %s": "Copying %s => %s",
 	"Symlinking %s => %s": "Symlinking %s => %s",
 	"Did you mean this?": "Did you mean this?",
-	"Could not find Titanium module id: %s version: %s platform: %s": "Could not find Titanium module id: %s version: %s platform: %s",
-	"Looking for Titanium module id: %s version: %s platform: %s": "Looking for Titanium module id: %s version: %s platform: %s",
-	"Detected module: %s %s @ %s": "Detected module: %s %s @ %s",
-	"Detecting modules in %s": "Detecting modules in %s",
 	"Installing module: %s": "Installing module: %s",
-	"Could not find Titanium plugin id: %s version: %s": "Could not find Titanium plugin id: %s version: %s",
-	"Looking for Titanium plugin id: %s version: %s": "Looking for Titanium plugin id: %s version: %s",
-	"Detected plugin: %s %s @ %s": "Detected plugin: %s %s @ %s",
+	"Detecting modules in %s": "Detecting modules in %s",
+	"Detected module: %s %s @ %s": "Detected module: %s %s @ %s",
+	"Looking for Titanium module id: %s version: %s platform: %s": "Looking for Titanium module id: %s version: %s platform: %s",
+	"Could not find Titanium module id: %s version: %s platform: %s": "Could not find Titanium module id: %s version: %s platform: %s",
+	"Detecting plugins in %s": "Detecting plugins in %s",
 	"Detected plugin: %s @ %s": "Detected plugin: %s @ %s",
-	"Detecting plugins in %s": "Detecting plugins in %s"
+	"Detected plugin: %s %s @ %s": "Detected plugin: %s %s @ %s",
+	"Looking for Titanium plugin id: %s version: %s": "Looking for Titanium plugin id: %s version: %s",
+	"Could not find Titanium plugin id: %s version: %s": "Could not find Titanium plugin id: %s version: %s"
 }
---
+++ locales/uk.js
@@ -0,0 +1,15 @@
+{
+	"Copying %s => %s": "Копіюю %s => %s",
+	"Symlinking %s => %s": "Створюю символьне посилання %s => %s",
+	"Did you mean this?": "Ви мали на увазі це?",
+	"Installing module: %s": "Встановлюю модуль: %s",
+	"Detecting modules in %s": "Виявляю модулі у %s",
+	"Detected module: %s %s @ %s": "Виявлено модуль: %s %s @ %s",
+	"Looking for Titanium module id: %s version: %s platform: %s": "Шукаю Titanium модуль id: %s версія: %s платформа: %s",
+	"Could not find Titanium module id: %s version: %s platform: %s": "Не вдалося знайти Titanium модуль id: %s версія: %s платформа: %s",
+	"Detecting plugins in %s": "Виявляю плагіни у %s",
+	"Detected plugin: %s @ %s": "Виявлено плагін: %s @ %s",
+	"Detected plugin: %s %s @ %s": "Виявлено плагін: %s %s @ %s",
+	"Looking for Titanium plugin id: %s version: %s": "Шукаю Titanium плагін id: %s версія: %s",
+	"Could not find Titanium plugin id: %s version: %s": "Не вдалося знайти Titanium плагін id: %s версія: %s"
+}
