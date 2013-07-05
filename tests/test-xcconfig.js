e188b6f702352dc0e19cfc6a57facf8fbc4e804f
Chris Barber <chris@cb1inc.com>
2013-07-05 09:12:45 -0700

    [TIMOB-11869] Added unit test for xcconfig parser.

---
+++ tests/test-xcconfig.js
@@ -0,0 +1,29 @@
+var appc = require('../index'),
+	path = require('path');
+
+describe('xcconfig', function () {
+	it('namespace exists', function () {
+		appc.should.have.property('xcconfig');
+		appc.xcconfig.should.be.a('function');
+	});
+
+	it('should load and parse a xcconfig file', function () {
+		var result = new appc.xcconfig(path.join(__dirname, 'resources', 'project.xcconfig'));
+		result.should.be.a('object');
+
+		result.should.eql({
+			TI_VERSION: '3.2.0',
+			TI_SDK_DIR: '/Users/chris/Library/Application Support/Titanium/mobilesdk/osx/$(TI_VERSION)/iphone',
+			TI_APPID: 'com.appcelerator.testapp2',
+			'OTHER_LDFLAGS[sdk=iphoneos*]': '$(inherited) -weak_framework iAd',
+			'OTHER_LDFLAGS[sdk=iphonesimulator*]': '$(inherited) -weak_framework iAd',
+			'#include "module': '"'
+		});
+	});
+
+	it('should fail to load non-existant xcconfig file', function () {
+		(function () {
+			new appc.xcconfig(path.join(__dirname, 'resources', 'does-not-exist.xcconfig'));
+		}).should.throw();
+	});
+});
