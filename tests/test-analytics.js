2e4378cd45fed48ab172d6589edb9b0041f0695b
Chris Barber <chris@cb1inc.com>
2013-07-08 19:02:01 -0700

    [TIMOB-11869] Added tests for progress and xml. Added copyright info to the tests.

--- tests/test-analytics.js
+++ tests/test-analytics.js
@@ -1,3 +1,10 @@
+/**
+ * node-appc - Appcelerator Common Library for Node.js
+ * Copyright (c) 2009-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
 var appc = require('../index'),
 	http = require('http'),
 	temp = require('temp');
